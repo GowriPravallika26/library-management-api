4e70f88daeaed8a59827c0c60f51d44f0637749d
GowriPravallika26 <147472849+GowriPravallika26@users.noreply.github.com>
2025-12-23 10:29:50 +0530

    Update transaction.controller.js

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -1,52 +1,16 @@
-// controllers/transactions.js
-const Transaction = require('../models/transaction.model');
-const Book = require('../models/book.model');
-const Member = require('../models/member.model');
+// routes/transactions.js
+const express = require('express');
+const router = express.Router();
+const {
+  getAllTransactions,
+  createTransaction,
+  updateTransaction,
+  deleteTransaction
+} = require('../controllers/transaction.controller');
 
-// Get all transactions
-const getAllTransactions = async (req, res) => {
-  try {
-    const transactions = await Transaction.findAll({
-      include: [Book, Member]
-    });
-    res.json(transactions);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
+router.get('/', getAllTransactions);
+router.post('/', createTransaction);
+router.put('/:id', updateTransaction);
+router.delete('/:id', deleteTransaction);
 
-// Create a new transaction (issue book)
-const createTransaction = async (req, res) => {
-  try {
-    const transaction = await Transaction.create(req.body);
-    res.status(201).json(transaction);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
-
-// Update transaction (return book)
-const updateTransaction = async (req, res) => {
-  try {
-    const transaction = await Transaction.findByPk(req.params.id);
-    if (!transaction) return res.status(404).json({ message: "Transaction not found" });
-    await transaction.update(req.body);
-    res.json(transaction);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
-
-// Delete transaction
-const deleteTransaction = async (req, res) => {
-  try {
-    const transaction = await Transaction.findByPk(req.params.id);
-    if (!transaction) return res.status(404).json({ message: "Transaction not found" });
-    await transaction.destroy();
-    res.json({ message: "Transaction deleted" });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
-
-module.exports = { getAllTransactions, createTransaction, updateTransaction, deleteTransaction };
+module.exports = router;
