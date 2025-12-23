105147f8da5236e44d5fe18caa877b56750bcb21
GowriPravallika26 <147472849+GowriPravallika26@users.noreply.github.com>
2025-12-23 10:30:48 +0530

    Update transaction.model.js

--- src/models/transaction.model.js
+++ src/models/transaction.model.js
@@ -1,4 +1,4 @@
-// models/transactionModel.js
+// models/transaction.model.js
 const { DataTypes } = require('sequelize');
 const sequelize = require('../db');
 const Book = require('./book.model');
@@ -7,23 +7,32 @@ const Member = require('./member.model');
 const Transaction = sequelize.define('Transaction', {
   issueDate: {
     type: DataTypes.DATE,
+    allowNull: false,
     defaultValue: DataTypes.NOW
   },
+
+  dueDate: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+
   returnDate: {
     type: DataTypes.DATE,
     allowNull: true
   },
+
   status: {
     type: DataTypes.ENUM('issued', 'returned'),
+    allowNull: false,
     defaultValue: 'issued'
   }
 });
 
 // Associations
-Member.hasMany(Transaction);
+Member.hasMany(Transaction, { foreignKey: { allowNull: false } });
 Transaction.belongsTo(Member);
 
-Book.hasMany(Transaction);
+Book.hasMany(Transaction, { foreignKey: { allowNull: false } });
 Transaction.belongsTo(Book);
 
 module.exports = Transaction;
